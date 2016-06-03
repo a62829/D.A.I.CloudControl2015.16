6ffc22ae99292958829f95774562967b312e7153
ZeRocha <ZeRocha@DESKTOP-K2SNIV0>
2016-06-03 21:29:30 +0100

    FINISH!!!!

---
+++ ajuuminho/Ws/c27EdicaoAdministradorJudicial.asmx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using ajuUminho.App_Code;
+using System.Data;
+
+namespace ajuUminho.Ws
+{
+    /// <summary>
+    /// Summary description for c27EdicaoAdministradorJudicial
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class c27EdicaoAdministradorJudicial : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public void editarAdministradorJudicial(string nome, string morada, string codPostal, string localidade, string email,
+                    string telefone, string telemovel, string fax, string cc, string iban, string nif, string lastChangeBy)
+        {
+            //// efetuar controlos
+            //d21RepresentanteLegal rl = new d21RepresentanteLegal();
+            //var id = rl.getID(cc);
+            //if (rl.ccUnique(cc, id) == true)
+            //{
+            //    rl.setRepresentanteLegal(nome, morada, codPostal, localidade, email, telefone, telemovel, fax, cc, iban, nif, lastChangeBy, id);
+            //}
+
+        }
+
+        [WebMethod]
+        public void criarAdministradorJudicial(string nome, string morada, string codPostal, string localidade, string email,
+            string telefone, string telemovel, string fax, string cc, string iban, string nif, string lastChangeBy)
+        {
+            d25AdministradorJudicial aj = new d25AdministradorJudicial();
+            aj.guardar(nome, morada, codPostal, localidade, email, telefone, telemovel, fax, cc, iban, nif, lastChangeBy);
+
+        }
+
+        //[WebMethod]
+        //public DataTable listarRepresentantesLegais()
+        //{
+        //    d21RepresentanteLegal rl = new d21RepresentanteLegal();
+        //    DataTable dt = rl.getListaRepresentanteLegal();
+        //    return dt;
+        //}
+
+        [WebMethod]
+        public d25AdministradorJudicialDto getAdministradorJudicial(string id)
+        {
+            d25AdministradorJudicialDto ajdto = new d25AdministradorJudicialDto();
+            return ajdto.getAdministradorJudicial(id);
+        }
+
+        [WebMethod]
+        public Dictionary<String, String> getListaAdministradorJudicial()
+        {
+
+            d25AdministradorJudicialDto ajdto = new d25AdministradorJudicialDto();
+            DataTable dt = ajdto.getListaAdministradorJudicial();
+            Dictionary<String, String> lista = new Dictionary<String, String>();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                lista.Add(Convert.ToString(dt.Rows[i]["id"]), Convert.ToString(dt.Rows[i]["nome"]));
+            }
+            return lista;
+
+        }
+    }
+}
