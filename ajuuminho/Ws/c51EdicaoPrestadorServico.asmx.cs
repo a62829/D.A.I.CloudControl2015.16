ed1f710d7490c3459b9786699574abb99f020d23
ZeRocha <ZeRocha@DESKTOP-K2SNIV0>
2016-06-04 12:44:09 +0100

    WS TOTAL BUG FIX

---
+++ ajuuminho/Ws/c51EdicaoPrestadorServico.asmx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using ajuUminho.App_Code;
+using System.Data;
+
+namespace ajuUminho.Ws
+{
+    /// <summary>
+    /// Summary description for c51EdicaoPrestadorServico
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class c51EdicaoPrestadorServico : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public void editarPrestadorServico(string nome, string morada, string codPostal, string localidade, string email,
+                                   string telefone, string telemovel, string fax, string cc, string iban, string nif, string lastChangeBy)
+        {
+            //// efetuar controlos
+            //d21RepresentanteLegal rl = new d21RepresentanteLegal();
+            //var id = rl.getID(cc);
+            //if (rl.ccUnique(cc, id) == true)
+            //{
+            //    rl.setRepresentanteLegal(nome, morada, codPostal, localidade, email, telefone, telemovel, fax, cc, iban, nif, lastChangeBy, id);
+            //}
+
+        }
+
+        [WebMethod]
+        public void criarPrestadorServico(string nome, string morada, string codPostal, string localidade, string email,
+            string telefone, string telemovel, string fax, string cc, string iban, string nif, string lastChangeBy)
+        {
+            d49PrestadorServico ps = new d49PrestadorServico();
+            ps.guardar(nome, morada, codPostal, localidade, email, telefone, telemovel, fax, cc, iban, nif, lastChangeBy);
+
+        }
+
+        //[WebMethod]
+        //public DataTable listarRepresentantesLegais()
+        //{
+        //    d21RepresentanteLegal rl = new d21RepresentanteLegal();
+        //    DataTable dt = rl.getListaRepresentanteLegal();
+        //    return dt;
+        //}
+
+        [WebMethod]
+        public d49PrestadorServicoDto getPrestadorServico(string id)
+        {
+            d49PrestadorServicoDto psdto = new d49PrestadorServicoDto();
+            return psdto.getPrestadorServico(id);
+        }
+
+        [WebMethod]
+        public Dictionary<String, String> getListaPrestadorServico()
+        {
+
+            d49PrestadorServicoDto pstdo = new d49PrestadorServicoDto();
+            DataTable dt = pstdo.getListaPrestadorServico();
+            Dictionary<String, String> lista = new Dictionary<String, String>();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                lista.Add(Convert.ToString(dt.Rows[i]["id"]), Convert.ToString(dt.Rows[i]["nome"]));
+            }
+            return lista;
+
+        }
+    }
+}
