ad45ea5b6ca9f16bcca1897e910d539ca28ce189
José Rocha <a70074@uminho.pt>
2016-06-14 22:43:19 +0100

    OKOKOK

--- ajuuminho/controls/administracao/i3AdministradorUtilizadores.ascx.cs
+++ ajuuminho/controls/administracao/i3AdministradorUtilizadores.ascx.cs
@@ -13,5 +13,60 @@ namespace ajuUminho.controls.administracao
         {
 
         }
+
+        protected void TabCriarUtilizador_Click(object sender, EventArgs e)
+        {
+            ListBoxVisibility.Visible = false;
+            ContentDetailsBox.Attributes.Add("class", "InsideViewsDetailsBox2");
+            ViewBoxVisible.Visible = true;
+            ButtonCriarID.Visible = true;
+            ButtonEditarID.Visible = false;
+            ButtonEliminarID.Visible = false;
+            TabCriarUtilizadorID.CssClass = "Clicked";
+            TabEditarUtilizadorID.CssClass = "Initial";
+            TabEliminarUtilizadorID.CssClass = "Initial";
+        }
+
+        protected void TabEditarUtilizador_Click(object sender, EventArgs e)
+        {
+            ListBoxVisibility.Visible = true;
+            ContentDetailsBox.Attributes.Add("class", "InsideViewsDetailsBox");
+            ViewBoxVisible.Visible = true;
+            ButtonCriarID.Visible = false;
+            ButtonEditarID.Visible = true;
+            ButtonEliminarID.Visible = false;
+            TabCriarUtilizadorID.CssClass = "Initial";
+            TabEditarUtilizadorID.CssClass = "Clicked";
+            TabEliminarUtilizadorID.CssClass = "Initial";
+        }
+
+        protected void TabEliminarUtilizador_Click(object sender, EventArgs e)
+        {
+            ListBoxVisibility.Visible = true;
+            ContentDetailsBox.Attributes.Add("class", "InsideViewsDetailsBox");
+            ViewBoxVisible.Visible = true;
+            ButtonCriarID.Visible = false;
+            ButtonEditarID.Visible = false;
+            ButtonEliminarID.Visible = true;
+            TabCriarUtilizadorID.CssClass = "Initial";
+            TabEditarUtilizadorID.CssClass = "Initial";
+            TabEliminarUtilizadorID.CssClass = "Clicked";
+        }
+
+        protected void ButtonCriarID_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonEditarID_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonEliminarID_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
