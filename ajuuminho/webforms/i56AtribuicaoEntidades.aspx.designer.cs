89b86ee20f9c8770d2e676c0add63b51a0766e4c
José Rocha <a70074@uminho.pt>
2016-06-19 11:39:08 +0100

    x

--- ajuuminho/webforms/i56AtribuicaoEntidades.aspx.designer.cs
+++ ajuuminho/webforms/i56AtribuicaoEntidades.aspx.designer.cs
@@ -58,13 +58,22 @@ namespace ajuUminho.webforms {
         protected global::System.Web.UI.WebControls.DropDownList DropDownListEntidadesID;
         
         /// <summary>
-        /// RadioButtonList1 control.
+        /// RadioButtons control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButtonList RadioButtonList1;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl RadioButtons;
+        
+        /// <summary>
+        /// RadioButtonListID control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList RadioButtonListID;
         
         /// <summary>
         /// ListBoxEntidadesID control.
